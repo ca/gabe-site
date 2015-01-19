3ecb853687ab37f9256cddcde1c8acdb248938e4
Alex Vallorosi <reddog9287@aim.com>
2015-01-18 22:17:48 -0500

    putting the server into prod mode

--- app.py
+++ app.py
@@ -10,6 +10,4 @@ def projects(name=None):
 	return render_template(name+'.html')
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run()
-    # app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=80)
